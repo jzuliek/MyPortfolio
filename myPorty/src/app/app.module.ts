e14f38a2327160dd19d3f47055d63a9fad8d7908
jzuliek <jzuliek@gmail.com>
2020-02-06 22:23:01 -0800

    start

--- myPorty/src/app/app.module.ts
+++ myPorty/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {CardModule} from 'primeng/card';
+import {TabViewModule} from 'primeng/tabview';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    CardModule,
+    TabViewModule
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ myPorty/src/app/pages/landing/landing.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
